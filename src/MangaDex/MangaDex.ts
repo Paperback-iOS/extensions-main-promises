46dd7560fa82b8a1e66bb967fdf01711d9a623db
Pogogo007 <Francisd246@hotmail.com>
2021-01-14 21:42:34 -0500

    Update MangaDex.ts

--- src/MangaDex/MangaDex.ts
+++ src/MangaDex/MangaDex.ts
@@ -42,7 +42,7 @@ export const MangaDexInfo: SourceInfo = {
   description: 'Overwrites SafeDex,unlocks all mangas MangaDex has to offer and loads slightly faster. supports notifications',
   icon: 'icon.png',
   name: 'MangaDex Unlocked',
-  version: '2.0.2',
+  version: '2.0.3',
   authorWebsite: 'https://github.com/Pogogo007/extensions-main-promises',
   websiteBaseURL: MANGADEX_DOMAIN,
   hentaiSource: false,
