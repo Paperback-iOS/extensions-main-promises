6807ecb9cda9abd1c32dd6ca4d91d79d678a50df
Faizan Durrani <faizandurrani324@gmail.com>
2023-05-01 03:42:01 +0500

    Bump Version

--- src/Paperback/Paperback.ts
+++ src/Paperback/Paperback.ts
@@ -50,7 +50,7 @@ import {
 //  - search method which is called even if the user search in an other source
 
 export const PaperbackInfo: SourceInfo = {
-    version: "1.2.9",
+    version: "1.2.10",
     name: "Paperback",
     icon: "icon.png",
     author: "Lemon | Faizan Durrani",
